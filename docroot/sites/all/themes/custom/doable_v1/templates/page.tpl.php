5f386ccd46a112f9891f2dcf8df27a10cbbb6898
Willy Karam <wk@willykaram.com>
2014-09-12 15:59:20 -0400

    Initial commit, themes

---
+++ docroot/sites/all/themes/custom/doable_v1/templates/page.tpl.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728148
+ */
+?>
+
+<div id="page">
+
+  <header class="header" id="header" role="banner">
+
+
+    <?php print render($page['header']); ?>
+
+  </header>
+
+  <div id="main">
+
+    <div id="content" class="column" role="main">
+      <?php print render($page['highlighted']); ?>
+      <?php print $breadcrumb; ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
+    </div>
+
+    <div id="navigation">
+
+      <?php print render($page['navigation']); ?>
+
+    </div>
+
+    <?php
+      // Render the sidebars to see if there's anything in them.
+      $sidebar_first  = render($page['sidebar_first']);
+      $sidebar_second = render($page['sidebar_second']);
+    ?>
+
+    <?php if ($sidebar_first || $sidebar_second): ?>
+      <aside class="sidebars">
+        <?php print $sidebar_first; ?>
+        <?php print $sidebar_second; ?>
+      </aside>
+    <?php endif; ?>
+
+  </div>
+
+  <?php print render($page['footer']); ?>
+
+</div>
+
+<?php print render($page['bottom']); ?>
