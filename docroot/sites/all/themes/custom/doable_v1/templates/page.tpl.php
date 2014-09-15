0d487271b29d678a8e4d6f155da132dd92233444
Willy Karam <wk@willykaram.com>
2014-09-15 01:49:34 -0400

    Placeholder page theming

--- docroot/sites/all/themes/custom/doable_v1/templates/page.tpl.php
+++ docroot/sites/all/themes/custom/doable_v1/templates/page.tpl.php
@@ -21,7 +21,6 @@
 
     <div id="content" class="column" role="main">
       <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
