804ebbbffe6711ee7edbd8959167ede56b670bcc
Willy Karam <admin@willykaram.com>
2014-11-19 03:58:10 -0600

    Adding Lift

---
+++ sites/all/modules/contrib/acquia_lift/js/acquia_lift_queue.js
@@ -0,0 +1,52 @@
+(function ($) {
+
+  var queueIsProcessing = false;
+  /**
+   * Actually trigger the Acquia Lift queue processing and let
+   * Drupal handle any command results.
+   */
+  var processQueue = function() {
+    if (queueIsProcessing) {
+      return;
+    }
+    queueIsProcessing = true;
+    var queue_url = Drupal.settings.basePath + 'acquia_lift/queue';
+    $.ajax({
+      url: queue_url,
+      type: "POST",
+      success: function (response, status, jqXHR) {
+        var processed = false;
+        // Process any Drupal commands returned.
+        for (var i in response) {
+          if (response.hasOwnProperty(i) && response[i]['command'] && Drupal.ajax.prototype.commands[response[i]['command']]) {
+            Drupal.ajax.prototype.commands[response[i]['command']](Drupal.ajax.prototype, response[i], status);
+            processed = true;
+          }
+        }
+        if (processed) {
+          Drupal.attachBehaviors();
+        }
+        $(document).trigger('acquiaLiftQueueSyncComplete');
+        queueIsProcessing = false;
+      }
+    });
+  }
+
+  /**
+   * Run the queue processing if the settings indicate to do so.
+   */
+  Drupal.behaviors.acquia_lift_queue = {
+    attach: function(context, settings) {
+      if (Drupal.settings.acquia_lift && Drupal.settings.acquia_lift.sync_queue) {
+        processQueue();
+      }
+    }
+  };
+
+  /**
+   * A Drupal AJAX command to trigger the queue processing.
+   */
+  Drupal.ajax.prototype.commands.acquia_lift_process_queue = function (ajax, response, status) {
+    processQueue();
+  };
+}(Drupal.jQuery));
---
+++ sites/all/modules/contrib/acquia_lift/js/navbar/navbar-tableheader.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Provide displacement information sticky tableheaders.
+ */
+
+(function ($, Drupal, displace) {
+
+"use strict";
+
+Drupal.navbar = Drupal.navbar || {};
+
+$.extend(Drupal.navbar, {
+  height: function () {
+    return displace().top;
+  }
+});
+
+}(Drupal.jQuery, Drupal, Drupal.displace));
